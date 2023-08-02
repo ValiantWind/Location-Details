ece1380b23dfa3092401a394d932deff280ca061
ValiantWind <83314733+ValiantWind@users.noreply.github.com>
2023-08-02 17:41:41 +0000

    API & UX Changes
    Changed the api used to fetch the IP to my very own and added a button that reveals the ip instead of viewing it when the page loads

--- js/device.js
+++ js/device.js
@@ -1,3 +1,5 @@
 let networkType = document.getElementById("NetworkType");
+let deviceMemory = document.getElementById("deviceMemory")
 
 networkType.innerHTML = "Network Type: " + navigator.connection.type;
+deviceMemory.innerHTML = `Device Memory: At least ${navigator.deviceMemory} GiB of RAM`
